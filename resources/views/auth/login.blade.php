614f9e1e976ff99e8896e4d15d82c9839235a621
Novan <kevinnovantino@gmail.com>
2026-07-14 15:11:21 +0700

    Create Login Page UI Perubahan di login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,128 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    @vite(['resources/css/app.css'])
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+    <title>Login</title>
+</head>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+<body class="bg-white-100 font-['Poppins']">
+
+    <!-- Container Utama -->
+    <div class="flex min-h-screen">
+
+        <!-- ================= LEFT SIDE ================= -->
+        <div class="relative w-1/2 bg-white">
+
+        <!-- Bagian atas  -->
+         <div class="flex flex-col items-center pt-20">
+
+         <!-- Judul -->
+
+            <h1 class="text-center text-[44px] font-bold uppercase leading-tight text-black">
+                 Direktorat Jenderal<br>
+                 Hortikultura
+            </h1>
+            
+        <!-- Logo  -->
+            <img
+                src="{{ asset('logo-kementrian-pertanian.png') }}"
+                alt="Logo Kementrian Pertanian"
+                class="mx-auto mt-20 w-44">
+
+        </div>  
+
+        <!-- Ilustrasi -->
+            <img
+                src="{{ asset('ilustrasi-petani-login.png') }}"
+                alt="Ilustrasi Login Page"
+                class="absolute bottom-0 -right-25 w-[1100px] max-w-none opacity-70">
+
         </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+        <!-- ================= RIGHT SIDE ================= -->
+        <div class="relative z-10 w-1/2 bg-green-700 rounded-l-[40px] overflow-hidden">
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+            <!-- Dekorasi Lingkarn Login Page -->
+                <div class="absolute -right-24 -bottom-24 h-64 w-64 rounded-full border-[25px] border-green-500 opacity-40"></div>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                <div class="absolute -right-10 -bottom-10 h-40 w-40 rounded-full border-[18px] border-green-400 opacity-30"></div>
+
+            <!-- Container Isi -->
+            <div class="flex flex-col items-center pt-28">
+
+                <!-- Judul -->
+                <h1 class="text-center text-white text-2xl font-bold uppercase leading-tight">
+                    Sistem Informasi <br>
+                    Pemantauan Komoditas
+                </h1>
+
+                <!-- Login Card -->
+                <div class="mt-12 w-[430px] rounded-[32px] bg-white p-12 shadow-2xl">
+
+                    <!-- Heading -->
+                    <h2 class="text-[40px] font-bold text-gray-900">
+                        Hello!
+                    </h2>
+
+                    <p class="mt-2 text-gray-500 text-sm">
+                        Sign In to get started
+                    </p>
+
+                    <!-- Form -->
+                    <form class="mt-8">
+
+                        <!-- Email -->
+                        <div>
+                            <input
+                                type="email"
+                                placeholder="Email Address"
+                                class="w-full rounded-full border border-gray-300 px-6 py-4 outline-none focus:border-green-700 focus:ring-2 focus:ring-green-700"
+                            >
+                        </div>
+
+                        <!-- Password -->
+                        <div class="mt-5">
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                class="mt-5 w-full rounded-full border border-gray-300 px-6 py-4 text-sm outline-none transition focus:border-green-700 focus:ring-2 focus:ring-green-700"
+                            >
+                        </div>
+
+                        <!-- Remember Me -->
+                        <div class="mt-5 flex items-center">
+                            <input
+                                id="remember"
+                                type="checkbox"
+                                class="h-4 w-4 rounded border-gray-300 text-green-700 focus:ring-green-700"
+                            >
+                            <label for="remember" class="ml-2 text-sm text-gray-600">
+                                Remember me
+                            </label>
+                        </div>
+                        <!-- Button -->
+                        <button
+                            type="submit"
+                            class="mt-10 h-14 w-full rounded-full bg-[#2E7D32] text-lg font-semibold text-white transition hover:bg-green-800"
+                        >
+                            Login
+                        </button>
+
+                    </form>
+
+                </div>
+
+            </div>
+
         </div>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
+    </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</body>
+</html>
