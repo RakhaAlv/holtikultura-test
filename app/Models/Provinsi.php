d157d5c1ce8e215b8cf1c4d83d0898462b62889b
Rakha Aljiva Prabaswara <rakhaaljiva23@gmail.com>
2026-07-16 11:39:56 +0700

    relasi tabel database

--- app/Models/Provinsi.php
+++ app/Models/Provinsi.php
@@ -7,8 +7,28 @@ use Illuminate\Database\Eloquent\Model;
 class Provinsi extends Model
 {
     protected $table = 'provinsi';
+
+    // ID diisi manual dengan Kode BPS, bukan auto-increment
     public $incrementing = false;
     protected $keyType = 'int';
     public $timestamps = false;
-    protected $guarded = [];
+
+    protected $fillable = ['id', 'nama_prov'];
+
+    // --- RELASI KE BAWAH ---
+    public function kabupaten()
+    {
+        return $this->hasMany(Kabupaten::class, 'provinsi_id');
+    }
+
+    // --- RELASI TRANSAKSI ---
+    public function target()
+    {
+        return $this->hasMany(Target::class, 'provinsi_id');
+    }
+
+    public function realisasi()
+    {
+        return $this->hasMany(Realisasi::class, 'provinsi_id');
+    }
 }
--- app/Models/Satuan.php
+++ app/Models/Satuan.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Satuan extends Model
 {
-    //
+    protected $table = 'satuan';
+    public $timestamps = false;
+
+    protected $fillable = ['nama_satuan'];
+
+    public function target()
+    {
+        return $this->hasMany(Target::class, 'satuan_id');
+    }
+
+    public function realisasi()
+    {
+        return $this->hasMany(Realisasi::class, 'satuan_id');
+    }
 }
