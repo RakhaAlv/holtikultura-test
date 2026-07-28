873d6b90dfcb4a97c031f63040d3f2d62316b0b2
AsyRio <theriohotasy@gmail.com>
2026-07-28 13:37:13 +0700

    filter dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -19,7 +19,9 @@
 
     {{-- Rekap Table --}}
     <div class="rounded-[28px] bg-white p-8 shadow-[0_8px_30px_rgba(0,0,0,0.08)]">
-        @include('dashboard.partials.recap-table')
+        <div id="rekapTable">
+            @include('dashboard.partials.recap-table')
+        </div>
     </div>
 
     {{-- Chart --}}
